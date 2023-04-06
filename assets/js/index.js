89d888c9a289f33cf808d3aed71901c58a7824d2
Sajad pp <96676224+mrsajadpp@users.noreply.github.com>
2023-04-06 20:09:29 +0530

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,3 +9,37 @@ menuBtn.addEventListener("click", () => {
         menuBtn.innerHTML = '<span class="material-icons">close</span>';
     }
 })
+
+const changeClass = (className) => {
+    let elements = document.querySelectorAll('.nav_active');
+    elements.forEach(element => {
+        element.classList.remove('nav_active');
+    });
+    let el = document.querySelectorAll('.' + className);
+    console.log(className)
+    console.log(el)
+    el.forEach(element => {
+        element.classList.add('nav_active');
+    });
+}
+
+let changeNav = () => {
+    if (location.href.endsWith('#start')) {
+        return changeClass('home');
+    }
+    if (location.href.endsWith('#aboutus')) {
+        return changeClass('aboutus');
+    }
+    if (location.href.endsWith('#services')) {
+        return changeClass('services');
+    }
+    if (location.href.endsWith('#projects')) {
+        return changeClass('projects');
+    }
+    if (location.href.endsWith('#contact')) {
+        return changeClass('contact');
+    }
+}
+
+window.onhashchange = changeNav;
+window.onload = changeNav;
