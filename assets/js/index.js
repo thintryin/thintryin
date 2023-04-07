1b9816cac5375c1226f22302973173cd1e386606
Sajad pp <96676224+mrsajadpp@users.noreply.github.com>
2023-04-07 15:54:55 +0530

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -43,3 +43,26 @@ window.addEventListener('resize', changeNav);
 window.addEventListener('load', changeNav);
 window.addEventListener('hashchange', changeNav);
 window.addEventListener('scroll', hideNav);
+
+$(document).ready(function () {
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
